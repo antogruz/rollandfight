2bd0f6de8d492b9212ff727f21a0233882fe5e40
Antoine Gruzelle <antogruz@gmail.com>
2018-09-06 09:34:06 +0200

    non-random generator

---
+++ generate.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import random
+
+fullsize = 25
+
+def main():
+    print(generate(0))
+
+def generate(n):
+    gap = random_gap()
+
+    if (n + gap >= fullsize):
+        return [gap] + random_bosses(n + gap)
+
+    return [gap, random_event(gap + n)] + generate(gap + n)
+
+def random_gap():
+    return 3
+
+def random_event(n):
+    return str(n) + "Blue"
+
+def random_bosses(n):
+    return [str(n) + "Boss"]
+
+main()
