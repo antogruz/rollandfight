ccf90caff52e4ee1cdf9fcbb6bd68ec5d5cf40cf
Antoine Gruzelle <antogruz@gmail.com>
2018-09-06 10:00:10 +0200

    generate random encounters

--- generate.py
+++ generate.py
@@ -23,14 +23,40 @@ def random_gap():
         return 2
     if draw < 9 :
         return 4
-    if random.randint(0, 1):
+    if coinflip():
         return 1
 
     return 5
 
-
 def random_event(n):
-    return str(n) + "Blue"
+    if coinflip():
+        return random_encounter(n)
+    else:
+        return random_location(n)
+
+def random_encounter(n):
+    outputs = how_many_outputs()
+    result = str(n) + "-"
+    for i in range(outputs):
+        if i > 0:
+            result += " | "
+        result += "monster"
+
+    return result
+
+def how_many_outputs():
+    draw = random.randint(0, 10)
+    if draw < 7 :
+        return 1
+    if draw < 10 :
+        return 2
+    return 3
+
+def random_location(n):
+    return str(n) + "location"
+
+def coinflip():
+    return random.randint(0, 1)
 
 def random_bosses(n):
     return [str(n) + "Boss"]
