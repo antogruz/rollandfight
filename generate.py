899b6a6c4435b38aca68ae03ec64a0fa5d6e25cc
Antoine Gruzelle <antogruz@gmail.com>
2018-09-06 09:43:52 +0200

    randomly choose gap, centered on 3

--- generate.py
+++ generate.py
@@ -16,7 +16,18 @@ def generate(n):
     return [gap, random_event(gap + n)] + generate(gap + n)
 
 def random_gap():
-    return 3
+    draw = random.randint(0, 9)
+    if draw < 5 :
+        return 3
+    if draw < 7 :
+        return 2
+    if draw < 9 :
+        return 4
+    if random.randint(0, 1):
+        return 1
+
+    return 5
+
 
 def random_event(n):
     return str(n) + "Blue"
